10cfd4051647e16534a1f78017b8698695dc0313
Andi Elliott <Andi.Elliott@os.uk>
2019-02-27 15:25:10 +0000

    fixed errors

--- jobs/app.py
+++ jobs/app.py
@@ -5,15 +5,15 @@ PATH = 'db/jobs.sqlite3'
 app = Flask(__name__)
 
 
-def open_connecton():
+def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        conenction = g.__connection = sqlite3.connect(PATH)
+        connection = g.__connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql, values = (), commit = False, single = False):
-    connection = open_connecton()
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
         results = connection.commit()
